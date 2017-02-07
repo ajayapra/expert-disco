a8434585382b8650c957ea60103a9e6b469bb788
ajayapra <ajayapra@mtu.edu>
2017-02-07 03:24:57 -0500

    added amcl dependencies of the random spawner

--- catkin_ws/src/patrolling/scripts/jackal_spawner.py
+++ catkin_ws/src/patrolling/scripts/jackal_spawner.py
@@ -8,7 +8,8 @@ def main():
     x=random.uniform(-10,10)
     y=random.uniform(-10,10)
     test = 'rosrun gazebo_ros spawn_model -urdf -model jackal -param robot_description -x "%f" -y "%f" -z 1.0'%(x,y)
-    print test
+    amcl = 'roslaunch patrolling amcl_custom.launch initial_x:="%f" initial_y:="%f"'%(x,y)
     os.system(test)
+    os.system(amcl)
 if __name__ == '__main__':
     main()
