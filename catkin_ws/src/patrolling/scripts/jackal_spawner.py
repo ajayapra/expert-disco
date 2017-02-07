3cf175b8c37dc9866b3a60693118ebf85339e64a
ajayapra <ajayapra@mtu.edu>
2017-02-07 00:21:49 -0500

    Adding semi working random spawn launch file

--- catkin_ws/src/patrolling/scripts/jackal_spawner.py
+++ catkin_ws/src/patrolling/scripts/jackal_spawner.py
@@ -5,15 +5,10 @@ import random
 import os
 
 def main():
-    # x = 5.0;
-    # y = -5.0;
     x=random.uniform(-10,10)
     y=random.uniform(-10,10)
     test = 'rosrun gazebo_ros spawn_model -urdf -model jackal -param robot_description -x "%f" -y "%f" -z 1.0'%(x,y)
     print test
     os.system(test)
-
-
-
 if __name__ == '__main__':
     main()
