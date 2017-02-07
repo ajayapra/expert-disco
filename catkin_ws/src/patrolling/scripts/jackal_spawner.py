29346d282e765f33b7882d055a2c4a53ed521fc0
mano <spalania@mtu.edu>
2017-02-07 02:21:06 -0600

    Working coordinate to pixel conversion.

--- catkin_ws/src/patrolling/scripts/jackal_spawner.py
+++ catkin_ws/src/patrolling/scripts/jackal_spawner.py
@@ -11,27 +11,28 @@ import os
 folder_path = '/home/mano/expert-disco/catkin_ws/src/patrolling/maps/map_dilated.pgm'
 img = cv2.imread(folder_path)
 
-x_init, y_init, _ = img.shape()
+x_init, y_init, _ = img.shape
 
 loop = True
 resolution = 0.02
-xmin = -20.88
-xmax = x_init*resolution + xmin
-ymin = -10.00
-ymax = y_init*resolution + ymin
+x_min = -10.88
+x_max = x_init * resolution - 20.0
+y_min = -8.00
+y_max = y_init * resolution - 10.0
 
 
 def coordinate_to_pixel(x, y):
-    global xmin
-    global ymin
-    ix = round((x-xmin)/0.02)
-    iy = round((y-ymin)/0.02)
+    global x_min
+    global y_min
+    ix = round((x - x_min) / 0.02)
+    iy = round((y - y_min) / 0.02)
     return ix, iy
 
 
-def scale_finder(ix, iy):
-    # ix, iy = coordinate_to_pixel(x, y)
+def scale_finder(x, y):
+    ix, iy = coordinate_to_pixel(x, y)
 
+    return False
 
 
 def spawn_check(x, y):
@@ -40,15 +41,16 @@ def spawn_check(x, y):
     else:
         return False
 
+
 def random_point():
     global loop
-    global xmin
-    global xmax
-    global ymin
-    global ymax
+    global x_min
+    global x_max
+    global y_min
+    global y_max
     while loop:
-        x = random.uniform(xmin, xmax)
-        y = random.uniform(ymin, ymax)
+        x = random.uniform(x_min+5, x_max+5)
+        y = random.uniform(y_min+3, y_max+3)
         loop = spawn_check(x, y)
     return x, y
 
@@ -56,10 +58,10 @@ def random_point():
 def main():
 
     x, y = random_point()
-    test = 'rosrun gazebo_ros spawn_model -urdf -model jackal -param robot_description -x "%f" -y "%f" -z 1.0'%(x,y)
-    amcl = 'roslaunch patrolling amcl_custom.launch initial_x:="%f" initial_y:="%f"'%(x,y)
-    os.system(test)
-    os.system(amcl)
+    # test = 'rosrun gazebo_ros spawn_model -urdf -model jackal -param robot_description -x "%f" -y "%f" -z 1.0' % (x, y)
+    # amcl = 'roslaunch patrolling amcl_custom.launch initial_x:="%f" initial_y:="%f"' % (x, y)
+    # os.system(test)
+    # os.system(amcl)
 
 if __name__ == '__main__':
     main()
