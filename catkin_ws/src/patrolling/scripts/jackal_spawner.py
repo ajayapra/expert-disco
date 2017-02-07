81a9b3714ad3f9e843732658726c97c812bc74b6
ajayapra <ajayapra@mtu.edu>
2017-02-06 22:54:27 -0500

    Adding dumb random spawner

--- catkin_ws/src/patrolling/scripts/jackal_spawner.py
+++ catkin_ws/src/patrolling/scripts/jackal_spawner.py
@@ -1,21 +1,17 @@
 #!/usr/bin/env python
 import rospy
 import random
-import roslaunch
+#import roslaunch
+import os
 
 def main():
-    x = 5.0;
-    y = 5.0;
-    #x=random.uniform(-10,10)
-    #y=random.uniform(-10,10)
-    package = "gazebo_ros"
-    executable = "spawn_model"
-    argset1 = "-urdf -model jackal -param robot_description -x %f -y %f -z 1.0"%(x,y)
-    print argset1
-    node = roslaunch.core.Node(package, executable, args=argset1)
-    launch = roslaunch.scriptapi.ROSLaunch()
-    launch.start()
-    process = launch.launch(node)
+    # x = 5.0;
+    # y = -5.0;
+    x=random.uniform(-10,10)
+    y=random.uniform(-10,10)
+    test = 'rosrun gazebo_ros spawn_model -urdf -model jackal -param robot_description -x "%f" -y "%f" -z 1.0'%(x,y)
+    print test
+    os.system(test)
 
 
 
